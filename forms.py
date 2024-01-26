d15a99bac1653fdc958074cde7eebcd30afe097d
IDGS802-18002135 <156369898+IDGS802-18002135@users.noreply.github.com>
2024-01-25 23:52:10 -0600

    FormulariosConClase MLJE 25-01-2024

---
+++ forms.py
@@ -0,0 +1,15 @@
+from wtforms import Form
+from wtforms import StringField, TextAreaField, TextField, SelectField,RadioField
+from wtforms import EmailField
+
+class UserForm(Form):
+    nombre=StringField("nombre")
+    email=EmailField("correo")
+    apaterno=TextField('apaterno')
+    materias=SelectField(choices=[('España','Esp',),('Mat','Matematicas'),
+                                  ('Ingles','ING')])
+    radios=RadioField('curso',choices=[("1","1"),("2","2"),("3","3")])
+
+    '''
+    FormulariosConClase
+    '''
